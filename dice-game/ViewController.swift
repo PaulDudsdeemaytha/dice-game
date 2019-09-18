d99b1bc68651411de0e4a2c4701902efe781d930
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-09-18 11:10:49 -0700

    Initial Commit

---
+++ dice-game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice-game
+//
+//  Created by Paul Dudsdeemaytha on 2019-09-18.
+//  Copyright © 2019 Paul Dudsdeemaytha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
